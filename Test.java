fca0b0c375f66f076db7544f04ea5f9413c4afe9
TANVIP-6M <tanvipty@gmail.com>
2026-03-08 12:00:59 +0530

    Change variable b from 20 to 25 in add method

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
     static void add()
     {
         int a=20;
-        int b=20;
+        int b=25;
         int res=a+b;
         System.out.println("the result is " +res);
     }
